9307bd1d261a2168ffb5616fd3920f431564c691
wheelercj <chrisw289@gmail.com>
2025-05-03 18:21:54 -0700

    Add the script

---
+++ main.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+import re
+
+
+venv_name_pattern: re.Pattern = re.compile(r"^\.?venv\d*$")
+
+docstring_pattern: re.Pattern = re.compile(r'""".*?"""', re.DOTALL)
+identifier_pattern: re.Pattern = re.compile(r"(?<!`)\b[^\W_]\w*__?\b(?!`)")
+
+
+def main():
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d", "--dir", metavar="starting-point", default=".", nargs=1, required=False
+    )
+
+    args: argparse.Namespace = parser.parse_args()
+    dir: str = args.dir
+
+    for dirpath, dirnames, filenames in os.walk(dir, topdown=True):
+        # remove any subfolders that should not be searched
+        i = len(dirnames)
+        for dirname in reversed(dirnames):
+            i -= 1
+            if venv_name_pattern.match(dirname):
+                del dirnames[i]
+            elif dirname in ("__pycache__", "dist", "build", "lib"):
+                del dirnames[i]
+
+        # search this folder
+        for file in filenames:
+            if file.endswith(".py"):
+                filepath: str = os.path.join(dirpath, file)
+                with open(filepath, "r", encoding="utf8") as file:
+                    content: str = file.read()
+
+                for match in docstring_pattern.finditer(content):
+                    docstring: str = match.group(0)
+                    for var_match in identifier_pattern.finditer(docstring):
+                        variable_name: str = var_match.group(0)
+                        start: int = match.start() + docstring.find(variable_name)
+                        line_number: int = content.count("\n", 0, start) + 1
+
+                        print(f"{filepath}:{line_number}: '{variable_name}' needs backticks")
+
+
+if __name__ == "__main__":
+    main()
